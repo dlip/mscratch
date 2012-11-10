bea9c615e339fc95e758c1f525e1afabd9b4d90e
Dane Lipscombe <dane@lipscombe.com.au>
2012-11-10 22:53:41 +0900

    Adding file saving

--- public/javascripts/MDoc.js
+++ public/javascripts/MDoc.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
-                var converter1 = Markdown.getSanitizingConverter();
-                var editor1 = new Markdown.Editor(converter1);
-                editor1.run();
-            }	);
+	var converter1 = Markdown.getSanitizingConverter();
+	var editor1 = new Markdown.Editor(converter1);
+	editor1.run();
+
+	$('#save').click(function() {
+		var data = {
+			text: $('#wmd-input').val()
+		}
+		$.post('/', data, function(text) {
+			alert(text.result);
+		});
+	});
+
+
+}	);
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,16 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'MDoc' });
+  fs = require("fs");
+  fs.readFile('doc/index.md', function(err, data) {
+  	res.render('index', { title: 'MDoc', text: data});
+  });
+};
+
+exports.save = function(req, res){
+  console.log(req.body);
+  fs = require("fs");
+  fs.writeFile('doc/index.md', req.body.text, function (err) {
+  	res.send({ result: err?"Error":"Ok" });
+  });
 };
